d7afb1f201c2974d170efef2ed1b404d2d6e9403
suadnesimi1 <suadnesimi12@gmail.com>
2021-05-12 18:26:51 +0200

    Initial Commit

--- src/main.java
+++ src/main.java
@@ -12,82 +12,24 @@ public class main {
         System.out.println("Please select one of the following options to continue!");
         System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit\n");
         int choice = s.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("How much money you will take");
-                    withdraw = s.nextInt();
-                    if (balance >= withdraw) {
-                        balance = balance - withdraw;
-                        System.out.println("Take your money");
-                    } else {
-                        System.out.println("Error transaction");
-                    }
-                    switch (choice) {
-                        case 1:
-                            System.out.println("Do you want to continue\nType 1 for yes or 0 for No");
-                            yes = s.nextInt();
-                            if (yes == 1) {
-                                System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit\n");
-                                choice = s.nextInt();
-                                switch (choice) {
-                                    case 1:
-                                        System.out.println("How much money you will take");
-                                        withdraw = s.nextInt();
-                                        if (balance >= withdraw) {
-                                            balance = balance - withdraw;
-                                            System.out.println("Take your money");
-                                        } else {
-                                            System.out.println("Error transaction");
-                                        }
-                                        break;
-                                    case 2:
-                                        System.out.println("How much money you want to deposit");
-                                        deposit = s.nextInt();
-                                        if ((balance >= deposit) || (balance <= deposit)) {
-                                            balance = balance + deposit;
-                                            System.out.println("Succefully transaction");
-
-                                        } else {
-                                            System.out.println("Error transaction");
-                                        }
-                                        break;
-                                    case 3:
-                                        System.out.println("Your total balance: " + balance);
-                                        break;
-                                    case 4:
-                                        System.out.println("Exit");
-                                        break;
-                                    default:
-                                        System.out.println("Wrong Choice, Please select of the options to continue");
-                                }
-                            } else {
-                                break;
-                            }
-                            System.out.println("\nYou can perfom maximum two transactions in a time.\nPlease take your card and try it again later!");
-
-                            break;
-                    }
-                    break;
-            }
-
-            switch (choice) {
-                case 2:
-                    System.out.println("How much money you want to deposit");
-                    deposit = s.nextInt();
-                    if ((balance >= deposit) || (balance <= deposit)) {
-                        balance = balance + deposit;
-                        System.out.println("Succefully transaction");
-
-                    } else {
-                        System.out.println("Error transaction");
-                    }
-                    while (true) {
-                        System.out.println("WAnt to continue? Yes =1, No =0");
-                        yes=s.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("How much money you will take");
+                withdraw = s.nextInt();
+                if (balance >= withdraw) {
+                    balance = balance - withdraw;
+                    System.out.println("Take your money");
+                } else {
+                    System.out.println("Error transaction");
+                }
+                switch (choice) {
+                    case 1:
+                        System.out.println("Do you want to continue\nType 1 for yes or 0 for No");
+                        yes = s.nextInt();
                         if (yes == 1) {
-                            System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit");
-                            choice=s.nextInt();
-                            switch(choice) {
+                            System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit\n");
+                            choice = s.nextInt();
+                            switch (choice) {
                                 case 1:
                                     System.out.println("How much money you will take");
                                     withdraw = s.nextInt();
@@ -117,33 +59,140 @@ public class main {
                                     break;
                                 default:
                                     System.out.println("Wrong Choice, Please select of the options to continue");
-
                             }
-
                         } else {
                             break;
                         }
-                        System.out.println("\nYou can perfom maximum two transactions in a time.\nPlease take your card and try it again later!");
+
 
                         break;
+                }
+                break;
+        }
 
+        switch (choice) {
+            case 2:
+                System.out.println("How much money you want to deposit");
+                deposit = s.nextInt();
+                if ((balance >= deposit) || (balance <= deposit)) {
+                    balance = balance + deposit;
+                    System.out.println("Successfully transaction");
+
+                } else {
+                    System.out.println("Error transaction");
+                }
+                while (true) {
+                    System.out.println("WAnt to continue? Yes =1, No =0");
+                    yes = s.nextInt();
+                    if (yes == 1) {
+                        System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit");
+                        choice = s.nextInt();
+                        switch (choice) {
+                            case 1:
+                                System.out.println("How much money you will take");
+                                withdraw = s.nextInt();
+                                if (balance >= withdraw) {
+                                    balance = balance - withdraw;
+                                    System.out.println("Take your money");
+                                } else {
+                                    System.out.println("Error transaction");
+                                }
+                                break;
+                            case 2:
+                                System.out.println("How much money you want to deposit");
+                                deposit = s.nextInt();
+                                if ((balance >= deposit) || (balance <= deposit)) {
+                                    balance = balance + deposit;
+                                    System.out.println("Succefully transaction");
+
+                                } else {
+                                    System.out.println("Error transaction");
+                                }
+                                break;
+                            case 3:
+                                System.out.println("Your total balance: " + balance);
+                                break;
+                            case 4:
+                                System.out.println("Exit");
+                                break;
+                            default:
+                                System.out.println("Wrong Choice, Please select of the options to continue");
+                                break;
+
+                        }
+                    } else {
+                        break;
                     }
                     break;
-            }
 
-            switch (choice) {
-                case 3:
-                    System.out.println("Your total Balance " + balance);
-
-                    break;
-            }
-            switch (choice) {
-                case 4:
-                    System.out.println("Enter " + 0 + " to Exit");
-                    break;
-            }
-
-            }
+                }
+                break;
         }
+
+        switch (choice) {
+            case 3:
+                System.out.println("Your total Balance " + balance);
+                while (true) {
+                    System.out.println("WAnt to continue? Yes =1, No =0");
+                    yes = s.nextInt();
+                    if (yes == 1) {
+                        System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit");
+                        choice = s.nextInt();
+                        switch (choice) {
+                            case 1:
+                                System.out.println("How much money you will take");
+                                withdraw = s.nextInt();
+                                if (balance >= withdraw) {
+                                    balance = balance - withdraw;
+                                    System.out.println("Take your money");
+                                } else {
+                                    System.out.println("Error transaction");
+                                }
+                                break;
+                            case 2:
+                                System.out.println("How much money you want to deposit");
+                                deposit = s.nextInt();
+                                if ((balance >= deposit) || (balance <= deposit)) {
+                                    balance = balance + deposit;
+                                    System.out.println("Succefully transaction");
+
+                                } else {
+                                    System.out.println("Error transaction");
+                                }
+                                break;
+                            case 3:
+                                System.out.println("Your total balance: " + balance);
+                                break;
+                            case 4:
+                                System.out.println("Exit");
+                                break;
+                            default:
+                                System.out.println("Wrong Choice, Please select of the options to continue");
+
+                        }
+
+                    } else {
+
+                        break;
+                    }
+                    break;
+
+                }
+                break;
+        }
+
+
+        switch (choice) {
+            case 4:
+                System.out.println("Enter " + 0 + " to Exit");
+
+
+                break;
+        }
+
+    }
+
+}
+
 
 
