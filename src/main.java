ff40ee0edbff13e38b1d948591f9fb993e61f5f6
suadnesimi1 <suadnesimi12@gmail.com>
2021-05-12 02:09:10 +0200

    Initial Commit

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+
         int balance = 100;
         int withdraw;
         int deposit;
@@ -80,8 +81,56 @@ public class main {
                     } else {
                         System.out.println("Error transaction");
                     }
+                    while (true) {
+                        System.out.println("WAnt to continue? Yes =1, No =0");
+                        yes=s.nextInt();
+                        if (yes == 1) {
+                            System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit");
+                            choice=s.nextInt();
+                            switch(choice) {
+                                case 1:
+                                    System.out.println("How much money you will take");
+                                    withdraw = s.nextInt();
+                                    if (balance >= withdraw) {
+                                        balance = balance - withdraw;
+                                        System.out.println("Take your money");
+                                    } else {
+                                        System.out.println("Error transaction");
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("How much money you want to deposit");
+                                    deposit = s.nextInt();
+                                    if ((balance >= deposit) || (balance <= deposit)) {
+                                        balance = balance + deposit;
+                                        System.out.println("Succefully transaction");
+
+                                    } else {
+                                        System.out.println("Error transaction");
+                                    }
+                                    break;
+                                case 3:
+                                    System.out.println("Your total balance: " + balance);
+                                    break;
+                                case 4:
+                                    System.out.println("Exit");
+                                    break;
+                                default:
+                                    System.out.println("Wrong Choice, Please select of the options to continue");
+
+                            }
+
+                        } else {
+                            break;
+                        }
+                        System.out.println("\nYou can perfom maximum two transactions in a time.\nPlease take your card and try it again later!");
+
+                        break;
+
+                    }
                     break;
             }
+
             switch (choice) {
                 case 3:
                     System.out.println("Your total Balance " + balance);
