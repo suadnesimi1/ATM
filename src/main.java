7b8db3bb949b395d21fdb42b6bbeb5997ac65be4
suadnesimi1 <suadnesimi12@gmail.com>
2021-05-12 18:37:31 +0200

    Initial Commit

--- src/main.java
+++ src/main.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 
-        int balance = 100;
-        int withdraw;
-        int deposit;
-        int yes = 1;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to our Bank ATM");
-        System.out.println("Please select one of the following options to continue!");
-        System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit\n");
-        int choice = s.nextInt();
+        int balance = 100; // declaration of variable and assign value
+        int withdraw; // declaration of variable
+        int deposit; // declaration of variable
+        int yes = 1; // declaration of variable and assign value
+        Scanner s = new Scanner(System.in); //scanner class is to get user input
+        System.out.println("Welcome to our Bank ATM"); // print this line
+        System.out.println("Please select one of the following options to continue!");//print this line
+        System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit\n");// print this line to selecet one of them
+        int choice = s.nextInt(); // nextInit() method used to scan token of the input integer
+        // read for switch statement to understand code
         switch (choice) {
             case 1:
                 System.out.println("How much money you will take");
@@ -82,7 +83,7 @@ public class main {
                     System.out.println("Error transaction");
                 }
                 while (true) {
-                    System.out.println("WAnt to continue? Yes =1, No =0");
+                    System.out.println("WAnt to continue?\n Yes =1, No =0");
                     yes = s.nextInt();
                     if (yes == 1) {
                         System.out.println("\n 1.Withdraw\n 2.Deposit\n 3.Balance\n 4.Exit");
